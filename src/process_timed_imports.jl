1b233d5ef5455f7cf66ce48e23e0cefa40925cb4
WhiffleFish <tylertronics@gmail.com>
2022-10-17 12:10:25 -0600

    fix ms split bug + sort timed_imports

--- src/process_timed_imports.jl
+++ src/process_timed_imports.jl
@@ -1,6 +1,3 @@
-# _path = abspath(joinpath(@__DIR__, "..", "test", "time_imports.txt"))
-# isfile(_path)
-
 function path2csv(path::AbstractString)
     split_path = split(path, '.')
     split_path[end] = "csv"
@@ -9,15 +6,19 @@ end
 
 function process_timed_imports(in_path::AbstractString, out_path=path2csv(in_path))
     lines = readlines(in_path)
-    stripped_lines = map(split.(lines, "ms")) do v
+    stripped_lines = map(split.(lines, " ms ")) do v
         v′ = strip.(v)
         v′[2] = first(split(v′[2]))
         v′
     end
+    sort!(stripped_lines, rev=true, by=v->parse(Float64, v[1]))
     csv_vals = join(join.(stripped_lines, ", "), '\n')
     final_str = "time(ms), package\n"*csv_vals
     open(out_path, "w") do f
         write(f, final_str)
     end
 end
+
+# _path = abspath(joinpath(@__DIR__, "..", "test", "time_imports.txt"))
+# isfile(_path)
 # process_timed_imports(_path)
